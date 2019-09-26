b229e1e6c31ce597c68539403a97ddc900400419
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-26 08:29:56 +0300

    init

--- SwiftUICorusel/ContentView.swift
+++ SwiftUICorusel/ContentView.swift
@@ -10,7 +10,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        
+        ZStack {
+            
+            VStack {
+                Text("Menu")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+            
+                ScrollView(.horizontal, showsIndicators: false) {
+                    
+                    HStack{
+                        ForEach(Range(1...4)) { iteration in
+                            
+                            GeometryReader { proxy in
+                                
+                                Image("food\(iteration)")
+                                .resizable()
+                                .scaledToFit()
+                                    .frame(width: 200, height: 200)
+                                .clipped()
+                                .padding()
+                                .shadow(radius: 3)
+                                    .background(Color.white)
+                                .shadow(radius: 3)
+                                    .rotation3DEffect(Angle(degrees: Double(proxy.frame(in: .global).midX) / 20), axis: (x: 100, y: -100, z: 100))
+                                
+                            }.frame(width: 200, height: 300)
+                            
+                        }
+                    }
+                }
+                
+                
+            }
+            
+            Spacer()
+            
+        }
+        .background(Image("bg").resizable().scaledToFill())
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
